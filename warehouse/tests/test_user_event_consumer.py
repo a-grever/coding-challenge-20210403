e1abfe25daa475ff53f02d61a74c43a9e1926a16
Andreas Grever <agrever@mailbox.org>
2021-04-05 22:56:50 +0200

    more tests

---
+++ warehouse/tests/test_user_event_consumer.py
@@ -0,0 +1,57 @@
+import json
+
+import pytest
+
+import user_event_consumer
+
+
+@pytest.fixture()
+def user_event():
+    return {
+        "id": "3e0a16993813100223734db91f7c79d9",
+        "event_type": "User Updated",
+        "username": "Zeus",
+        "user_email": "zeus@olympus.com",
+        "user_type": "Admin",
+        "organization_name": "God of War",
+        "plan_name": "Enterprise",
+        "received_at": "2020-12-08 04:03:02.759617",
+    }
+
+
+def test_callback(mocker, mock_engine, user_event):
+    mocker.patch.object(user_event_consumer, "get_pg_engine", return_value=mock_engine)
+    mock_ch = mocker.MagicMock()
+    mock_method = mocker.MagicMock()
+    user_event_consumer.callback(
+        ch=mock_ch, method=mock_method, properties=None, body=json.dumps(user_event).encode()
+    )
+    mock_engine.execute.assert_called_once()
+    mock_ch.basic_ack.assert_called_once_with(
+        delivery_tag=mock_method.delivery_tag
+    )
+
+
+def test_callback_reused_engine(mocker, monkeypatch, mock_engine, user_event):
+    monkeypatch.setattr(user_event_consumer, "engine", mock_engine)
+    mock_ch = mocker.MagicMock()
+    mock_method = mocker.MagicMock()
+    user_event_consumer.callback(
+        ch=mock_ch, method=mock_method, properties=None, body=json.dumps(user_event).encode()
+    )
+    mock_engine.execute.assert_called_once()
+    mock_ch.basic_ack.assert_called_once_with(
+        delivery_tag=mock_method.delivery_tag
+    )
+
+
+def test_main(mocker, monkeypatch):
+    monkeypatch.setenv("QUEUE_HOST", "test-queue")
+    mock_con = mocker.patch.object(user_event_consumer.pika, "BlockingConnection").return_value
+    mock_chan = mock_con.channel.return_value
+    user_event_consumer.main()
+
+    mock_chan.basic_consume.assert_called_once_with(
+        queue="user_events", on_message_callback=user_event_consumer.callback
+    )
+    mock_chan.queue_declare.assert_called_once_with(queue="user_events", durable=True)
---
+++ warehouse/tests/test_user_event_producer.py
@@ -0,0 +1,62 @@
+from io import StringIO
+import json
+
+import pytest
+
+import user_event_producer
+
+
+@pytest.fixture()
+def user_events():
+    return [
+        {
+            "id": "3e0a16993813100223734db91f7c79d9",
+            "event_type": "User Created",
+            "username": "Zeus",
+            "user_email": "zeus@olympus.org",
+            "user_type": "Admin",
+            "organization_name": "God of War",
+            "plan_name": "Enterprise",
+            "received_at": "2020-12-06 22:03:16.759617",
+        },
+        {
+            "id": "3e0a16993813100223734db91f7c79d9",
+            "event_type": "User Updated",
+            "username": "Zeus",
+            "user_email": "zeus@olympus.com",
+            "user_type": "Admin",
+            "organization_name": "God of War",
+            "plan_name": "Enterprise",
+            "received_at": "2020-12-08 04:03:02.759617",
+        },
+    ]
+
+
+def test_get_user_events(mocker, user_events):
+    mock_data_folder = mocker.patch.object(user_event_producer, "data_folder")
+    mock_org_sample = mock_data_folder / "events_sample.json"
+    mock_org_sample.exists.return_value = True
+    mock_org_sample.open.return_value = StringIO(json.dumps(user_events))
+    events = list(user_event_producer.get_user_events())
+    assert events == user_events
+
+
+def test_test_get_user_events_missing_file(mocker):
+    mock_data_folder = mocker.patch.object(user_event_producer, "data_folder")
+    mock_org_sample = mock_data_folder / "events_sample.json"
+    mock_org_sample.exists.return_value = False
+    with pytest.raises(RuntimeError):
+        list(user_event_producer.get_user_events())
+
+
+def test_main(mocker, monkeypatch, user_events):
+    monkeypatch.setenv("QUEUE_HOST", "test-queue")
+    mock_con = mocker.patch.object(user_event_producer.pika, "BlockingConnection").return_value
+    mocker.patch.object(user_event_producer, "get_user_events", return_value=user_events)
+    mock_chan = mock_con.channel.return_value
+    user_event_producer.main()
+
+    assert mock_chan.basic_publish.call_count == len(user_events)
+    mock_chan.queue_declare.assert_called_once_with(
+        queue="user_events", durable=True, exclusive=False
+    )
